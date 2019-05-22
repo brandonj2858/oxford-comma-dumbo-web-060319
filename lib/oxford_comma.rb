e146dac598538770c2e5a41763a3e2c821cda640
Brandon James <brandonj2858@gmail.com>
2019-05-22 19:37:45 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
 if array.length == 2 
   return "#{array[0]} and #{array[1]}"
 else if 2 < array.length
- array[-1].insert(2, "and ")
+ array[-1].insert(0, "and ")
 end
   return array.join(", ")
 end
